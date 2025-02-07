f3b3362acdbb8f808ad7c63456a1a6fcc0601d79
Kirill Pet <job.mori.1967@gmail.com>
2025-02-07 18:59:51 +0300

    completed-works pc

--- verstka/src/main.js
+++ verstka/src/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiperCertificates = new Swiper('.swiper-certificates', {
     // Optional parameters
     direction: 'horizontal',
     slidesPerView: 5.8,
@@ -31,4 +31,38 @@ const swiper = new Swiper('.swiper', {
         slidesPerView: 5.8,
       },
     }
+});
+
+const swiperWorks = new Swiper('.swiper-works', {
+  // Optional parameters
+  direction: 'horizontal',
+  spaceBetween: 16,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    660: {
+      slidesPerView: 3,
+    },
+    900: {
+      slidesPerView: 4,
+    },
+    1000: {
+      slidesPerView: 5.5,
+    },
+    1400: {
+      slidesPerView: 4,
+    },
+  }
 });
