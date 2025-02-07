ef1f5fa870353d7076f43bc756e9355c40541768
Kirill Pet <job.mori.1967@gmail.com>
2025-02-07 17:48:31 +0300

    certificates с адаптивностью

--- verstka/src/main.js
+++ verstka/src/main.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     slidesPerView: 5.8,
-    spaceBetween: 16,  
+    spaceBetween: 16,
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
@@ -13,4 +13,22 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      660: {
+        slidesPerView: 3,
+      },
+      900: {
+        slidesPerView: 4,
+      },
+      1000: {
+        slidesPerView: 5.5,
+      },
+      1400: {
+        slidesPerView: 5.8,
+      },
+    }
 });
