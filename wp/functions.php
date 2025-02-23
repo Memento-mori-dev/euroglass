b11cb322e44aa1f1cb28d79bd84021f36a0f3e9d
Kirill Pet <job.mori.1967@gmail.com>
2025-02-23 23:58:06 +0300

    auto title

--- wp/functions.php
+++ wp/functions.php
@@ -2,16 +2,27 @@
 
 // Стили и скрипты
 
+if (! function_exists('euroglass_setup')) {
+    function euroglass_setup(){
+        add_theme_support('title-tag');
+    }
+    add_action( 'after_setup_theme', 'euroglass_setup');
+}
+
+
+
 add_action( 'wp_enqueue_scripts', 'euroglass_scripts' );
 
 function euroglass_scripts() {
 	wp_enqueue_style( 'main-style', get_stylesheet_uri() );
     wp_enqueue_style( 'swiper-style', get_template_directory_uri() . '/assets/swiper-bundle.min.css', array('main-style'));
     wp_enqueue_style( 'euroglass-style', get_template_directory_uri() . '/assets/style.css', array('swiper-style'));
-
-    
+  
     wp_enqueue_script( 'swiper-script', get_template_directory_uri() . '/assets/script/swiper-bundle.min.js', array(), '1.0.0', true );
 	wp_enqueue_script( 'euroglass-script', get_template_directory_uri() . '/assets/script/main.js', array('swiper-script'), '1.0.0', true );
 }
+
+
+
 
 ?>
