6c6b97b6bfdd1a22aab3189afeaaa1819bf3c789
Kirill Pet <job.mori.1967@gmail.com>
2025-02-24 03:59:56 +0300

    сделал формы: расчет и вопросы

--- wp/php-content/catalog/form-card.php
+++ wp/php-content/catalog/form-card.php
@@ -2,21 +2,5 @@
     <p class="card-form__title">Lorem ipsum dolor sit amet</p>
     <p class="card-form__text">Lorem ipsum dolor sit amet consectetur. Ultricies nulla cras a sit urna lacus.</p>
 
-    <form action="" class="card-form__content">
-        <input type="text" class="input" placeholder="Имя">
-
-        <input type="text" class="input" placeholder="Ваш телефон">
-
-        <button class="file">
-            <p class="file__name">Прикрепить</p>
-            <p class="file__description">*docx, pdf, zip, rar</p>
-
-            <input type="file" name="" id="">
-        </button>
-
-        <a href="#" class="btn btn__transparent-white">Оставить заявку</a>
-
-        <p class="card-form__conditions">Я даю согласие на обработку своих персональных данных
-            в соответствии с <a href="#">политикой конфиденциальности</a>.</p>
-    </form>
+    <?=do_shortcode('[contact-form-7 id="261173a" title="Оставить заявку"]');?>
 </div>
