5d86890bb8f046158435ff18900ebcaa5652b885
Kirill Pet <job.mori.1967@gmail.com>
2025-02-24 22:33:51 +0300

    сделал сдедующий и предыдущий для галаереи

--- wp/single-gallery.php
+++ wp/single-gallery.php
@@ -11,7 +11,26 @@ $imagesUrl = $images['url'];
 $gallery = get_post_meta($id, 'images_prop');
 
 
+$my_posts = get_posts([
+  'category_name' => 'gallery',
+  'posts_per_page' => -1,
+  'order' => 'ASC',
+]);
 
+$preIdPost;
+$nextIdPost;
+
+foreach ($my_posts as $key => $value) {
+  if ($value->ID == $id) {
+    if ($my_posts[$key - 1]) {
+      $preIdPost = $my_posts[$key - 1]->ID;
+    }
+
+    if ($my_posts[$key + 1]) {
+      $nextIdPost = $my_posts[$key + 1]->ID;
+    }
+  }
+}
 ?>
 
 <div class="block">
@@ -25,9 +44,13 @@ $gallery = get_post_meta($id, 'images_prop');
           </div>
 
           <div class="gallery-banner__footer">
-                <!-- <a href="#" class="gallery-banner-footer__back">Предыдущий</a>
-
-            <a href="#" class="gallery-banner-footer__next">Следующий объект</a> -->
+            <?if ($preIdPost):?>
+              <a href="<?=get_permalink($preIdPost);?>" class="gallery-banner-footer__back">Предыдущий</a>
+            <? endif;?>
+            
+            <?if ($nextIdPost):?>
+              <a href="<?=get_permalink($nextIdPost);?>" class="gallery-banner-footer__next">Следующий объект</a>
+            <? endif;?>
           </div>
         </div>
       </div>
